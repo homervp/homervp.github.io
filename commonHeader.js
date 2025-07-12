05d51bb7ffc47490dc33a024609908de222d7307
Homer Papadatos-Vasilakis <homervp@gmail.com>
2025-07-12 16:55:10 +0300

    Initial push

---
+++ commonHeader.js
@@ -0,0 +1,40 @@
+document.write('\
+\
+\
+	<meta name="viewport" content="width=device-width, initial-scale=1">\
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">\
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>\
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>\
+<link rel="stylesheet" type="text/css" href="mainStyle.css">\
+\
+<div class="topnav">\
+	<row>\
+		<a class="jumbotron" href="/~vpapadatos/index.html">Homer Papadatos-Vasilakis</a>\
+	</row>\
+	<row>\
+		<a class="active" href="/~vpapadatos/index.html">Home</a>\
+		<a href="/~vpapadatos/TutorialCentral.html">Tutorials</a>\
+		<a href="/~vpapadatos/FlotillaWWII.html">Flotilla WWII</a>\
+		<a href="/~vpapadatos/Recommendations.html">Recommendations</a>\
+		<div class="dropdown">\
+			<button class="dropbtn">Projects</button>\
+			<div class="dropdown-content">\
+				<a href="/~vpapadatos/FlotillaWWII.html">Flotilla WWII</a>\
+				<a href="/~vpapadatos/SurvivalWasteland.html">Survival Wasteland</a>\
+				<a href="/~vpapadatos/TactiCode.html">TactiCode</a>\
+			</div>\
+		</div>\
+		<div class="dropdown">\
+			<button class="dropbtn">Film making</button>\
+			<div class="dropdown-content">\
+				<a href="/~vpapadatos/cinematography/film_making_and_writing_resources.html">Film making and writing resources</a>\
+				<a href="/~vpapadatos/cinematography/film_portfolio.html">Film portfolio</a>\
+			</div>\
+		</div>\
+		</div> \
+		<a href="Contact.html">Contact</a>\
+	</row>\
+</div>\
+\
+\
+');
---
+++ quotes.js
@@ -0,0 +1,106 @@
+﻿var quotes=[
+'“If our lives are already written, it would take a courageous man to change the script.” -Alan Wake, Alan Wake',
+'"What good is an intellect if you can\'t use it?" -Raiden, Metal Gear Rising: Revengeance',
+'"Technologies are invented to make our lives easier -- not our choices." -Adam Jensen, Deus Ex: Human Revolution',
+'"It is a sign of strength, to cry out against fate, rather than to bow one\'s head and succumb." -Gabriel Angelos, Warhammer 40,000: Dawn of War II',
+'“The right man in the wrong place can make all the difference in the world.” -G-Man, Half-Life 2',
+'"The hardest battles are fought in the mind, not with the sword." -Dillion, Hellblade: Senua\'s Sacrifice',
+'"Even the good leaders make poor decisions, it is the best leaders that take responsibility for them." -Kratos, God of War',
+'"Too many people have opinions on things they know nothing about. And the more ignorant they are, the more opinions they have." -Thomas Hildern, Fallout: New Vegas',
+'"A strong man doesn\'t need to read the future. He makes his own." -Solid Snake, Metal Gear Solid',
+'"Good men mean well. We just don’t always end up doing well." -Isaak Clarke, Dead Space 3',
+//10
+'"Who are you, that do not know your history?" -Ulysses, Fallout New Vegas',
+'“Even in dark times, we cannot relinquish the things that make us human.” –Khan, Metro 2033',
+'"If history only remembers one in a thousands of us, then the future will be filled with stories of who we were and what we did." -Battlefield 1',
+'"You can’t undo what you’ve already done, but you can face up to it." -Frank Coleridge, Silent Hill: Downpour',
+'"No matter how dark the night, morning always comes, and our journey begins anew." -Lulu, Final Fantasy X',
+'"We all make choices in life, but in the end our choices make us."-Andrew Ryan, Bioshock',
+//16
+
+'“When the door you have been knocking at finally swings open, you don’t ask why. You run through.” -Diane Lockhart, The Good Wife',
+'"Why do the smart people hide their learning? - To compensate for those who show off their learning" -Ashbaugh and Alicia Florick, The Good Wife',
+'"If you\'ve got something to brag about, usually you don\'t brag" -Robyn Burdine, The Good Wife',
+
+'"The wrong decision is better than indecision" -Tony Soprano, The Sopranos',
+
+
+//20
+'"Live now; make now always the most precious time. Now will never come again" -Jean-Luc Picard, Star Trek: The Next Generation',
+'“Insufficient facts always invite danger.” —Mr. Spock, Star Trek: The Original Series',
+'“You may find that having is not so pleasing a thing as wanting. This is not logical, but it is often true.” –Mr. Spock, Star Trek: The Original Series',
+'“In critical moments, men sometimes see exactly what they wish to see.” –Mr. Spock, Star Trek: The Original Series',
+'“You can use logic to justify almost anything. That’s its power. And its flaw.” –Cathryn Janeway, Star Trek: Voyager',
+'“They used to say that if Man was meant to fly, he’d have wings. But he did fly. He discovered he had to.” –James T. Kirk, Star Trek: The Original Series',
+'“If we’re going to be damned, let’s be damned for what we really are.” –Jean-Luc Picard, Star Trek: The Next Generation',
+'“Computers make excellent and efficient servants, but I have no wish to serve under them.” –Mr. Spock, Star Trek: The Original Series',
+'“It is possible to commit no mistakes and still lose. That is not weakness, that is life.”  –Jean-Luc Picard, Star Trek: The Next Generation',
+'"In this galaxy there’s a mathematical probability of three million Earth-type planets. And in the universe, three million million galaxies like this. \
+And in all that, and perhaps more...only one of each of us."-Dr. McCoy, Star Trek: The Original Series',
+//30
+
+//31
+'"A fraudulent intent, however carefully concealed at the outset, will generally, in the end, betray itself." -Livy',
+'"This above all makes history useful and desirable; it unfolds before our eyes a glorious record of exemplary actions." -Livy',
+'"It is easier to criticize than to correct our past errors." -Livy',
+'"Many difficulties which nature throws in our way, may be smoothed away by the exercise of intelligence." -Livy',
+'"Luck is of little moment to the great general, for it is under the control of his intellect and his judgment." -Livy',
+//35
+
+//36
+'"Mix a little foolishness with your serious plans. It is lovely to be silly at the right moment." -Horace',
+'"Adversity has the effect of eliciting talents, which in prosperous circumstances would have lain dormant." -Horace',
+'"Seize the day, and put the least possible trust in tomorrow." -Horace',
+'"To have a great man for a friend seems pleasant to those who have never tried it; those who have, fear it." -Horace',
+'"Words will not fail when the matter is well considered." -Horace',
+'"It is of no consequence of what parents a man is born, as long as he be a man of merit." -Horace',
+'"Fortune makes a fool of those she favors too much." -Horace',
+//43
+
+//44
+'"To show resentment at a reproach is to acknowledge that one may have deserved it." -Tacitus',
+'"Old things are always in good repute, present things in disfavor." -Tacitus',
+//45
+
+'"Common sense is not so common." -Voltaire',
+'"Appreciation is a wonderful thing: It makes what is excellent in others belong to us as well." -Voltaire',
+'"It is lamentable, that to be a good patriot one must become the enemy of the rest of mankind." -Voltaire',
+'"It is dangerous to be right in matters on which the established authorities are wrong." -Voltaire',
+'"Is there anyone so wise as to learn by the experience of others?" -Voltaire',
+'"A witty saying proves nothing." -Voltaire',
+'"Each player must accept the cards life deals him or her: but once they are in hand, he or she alone must decide how to play the cards in order to win the game." -Voltaire',
+'"The true triumph of reason is that it enables us to get along with those who do not possess it." -Voltaire',
+'"In the case of news, we should always wait for the sacrament of confirmation." -Voltaire',
+'"Originality is nothing but judicious imitation. The most original writers borrowed one from another." -Voltaire',
+'"I am very fond of truth, but not at all of martyrdom." -Voltaire',
+'"He must be very ignorant for he answers every question he is asked." -Voltaire',
+'"The progress of rivers to the ocean is not so rapid as that of man to error." -Voltaire',
+'"The instruction we find in books is like fire. We fetch it from our neighbours, kindle it at home, communicate it to others, and it becomes the property of all." -Voltaire',
+'"In every author let us distinguish the man from his works." -Voltaire',
+
+//46
+
+'"Don\'t wish it were easier, wish you were better."-Jim Rohn',
+//46
+
+'"Success is a lousy teacher. It seduces smart people into thinking they can\'t lose." -Bill Gates',
+'"It\'s fine to celebrate success but it\'s more important to heed the lessons of failure." -Bill Gates',
+'"We always overestimate the change that will occur in the next two years and underestimate the change that will occur in the next ten. Don\'t let yourself be lulled into inaction." -Bill Gates',
+'"The advandce of technology is based on making it fit in so that you don\'t really even notice it, so it\'s part of everyday life." -Bill Gates',
+'"Treatment without prevention is simply unsustainable." -Bill Gates',
+
+'"All that is gold does not glitter,\r\nNot all those who wander are lost;\r\nThe old that is strong does not wither,\r\nDeep roots are not reached by the frost." -J.R.R. Tolkien',
+'"(and) all I ask is a (tall) ship, and a star to steer her by" -John Masefield, Sea Fever (as quoted by Picard)'
+];
+
+function getQuote() {
+	document.getElementById("quoteDisp").innerHTML = "Paragraph changed.";
+	var numQuotes = quotes.length;
+	
+	var timeSec = Math.floor(Date.now() / 1000);
+	var timeRand = timeSec % numQuotes;
+	
+	var rand = Math.floor(Math.random() * numQuotes);
+	
+	document.getElementById("quoteDisp").innerHTML = quotes[timeRand];
+}
