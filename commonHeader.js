ea5ec15f69ecbb5db51874e58ca69de5e5558dfb
Homer Papadatos-Vasilakis <homervp@gmail.com>
2025-07-14 16:03:39 +0300

    Updated link roots and bio

--- commonHeader.js
+++ commonHeader.js
@@ -1,40 +1,58 @@
-document.write('\
-\
-\
-	<meta name="viewport" content="width=device-width, initial-scale=1">\
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">\
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>\
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>\
-<link rel="stylesheet" type="text/css" href="mainStyle.css">\
-\
-<div class="topnav">\
-	<row>\
-		<a class="jumbotron" href="/~vpapadatos/index.html">Homer Papadatos-Vasilakis</a>\
-	</row>\
-	<row>\
-		<a class="active" href="/~vpapadatos/index.html">Home</a>\
-		<a href="/~vpapadatos/TutorialCentral.html">Tutorials</a>\
-		<a href="/~vpapadatos/FlotillaWWII.html">Flotilla WWII</a>\
-		<a href="/~vpapadatos/Recommendations.html">Recommendations</a>\
-		<div class="dropdown">\
-			<button class="dropbtn">Projects</button>\
-			<div class="dropdown-content">\
-				<a href="/~vpapadatos/FlotillaWWII.html">Flotilla WWII</a>\
-				<a href="/~vpapadatos/SurvivalWasteland.html">Survival Wasteland</a>\
-				<a href="/~vpapadatos/TactiCode.html">TactiCode</a>\
-			</div>\
-		</div>\
-		<div class="dropdown">\
-			<button class="dropbtn">Film making</button>\
-			<div class="dropdown-content">\
-				<a href="/~vpapadatos/cinematography/film_making_and_writing_resources.html">Film making and writing resources</a>\
-				<a href="/~vpapadatos/cinematography/film_portfolio.html">Film portfolio</a>\
-			</div>\
-		</div>\
-		</div> \
-		<a href="Contact.html">Contact</a>\
-	</row>\
-</div>\
-\
-\
-');
+// Detect environment and set base URL
+const BASE_URL = (() => {
+  const host = window.location.hostname;
+  const path = window.location.pathname;
+
+  if (host.includes("example.com") && path.startsWith("/~user/")) {
+    return "/~user/";
+  }
+  if (host.includes("github.io")) {
+    return "/";
+  }
+  return "/";
+})();
+
+// Inject meta and styles into <head>
+document.addEventListener("DOMContentLoaded", () => {
+  document.head.insertAdjacentHTML("beforeend", `
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="${BASE_URL}mainStyle.css">
+  `);
+
+  // Inject navigation bar into <body>
+  document.body.insertAdjacentHTML("afterbegin", `
+    <div class="topnav">
+      <div class="row">
+        <a class="jumbotron" href="${BASE_URL}index.html">Homer Papadatos-Vasilakis</a>
+      </div>
+      <div class="row">
+        <a class="active" href="${BASE_URL}index.html">Home</a>
+        <a href="${BASE_URL}TutorialCentral.html">Tutorials</a>
+        <a href="${BASE_URL}FlotillaWWII.html">Flotilla WWII</a>
+        <a href="${BASE_URL}Recommendations.html">Recommendations</a>
+
+        <div class="dropdown">
+          <button class="dropbtn">Projects</button>
+          <div class="dropdown-content">
+            <a href="${BASE_URL}FlotillaWWII.html">Flotilla WWII</a>
+            <a href="${BASE_URL}SurvivalWasteland.html">Survival Wasteland</a>
+            <a href="${BASE_URL}TactiCode.html">TactiCode</a>
+          </div>
+        </div>
+
+        <div class="dropdown">
+          <button class="dropbtn">Film making</button>
+          <div class="dropdown-content">
+            <a href="${BASE_URL}cinematography/film_making_and_writing_resources.html">Film making and writing resources</a>
+            <a href="${BASE_URL}cinematography/film_portfolio.html">Film portfolio</a>
+          </div>
+        </div>
+
+        <a href="${BASE_URL}Contact.html">Contact</a>
+      </div>
+    </div>
+  `);
+});
